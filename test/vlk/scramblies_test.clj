7049400d0022ef1021d33b07ad9d2c5deef32f12
Peter Vlk <petervlk86@gmail.com>
2021-05-06 12:40:45 +0200

    add tests

--- test/vlk/scramblies_test.clj
+++ test/vlk/scramblies_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [vlk.scramblies :refer :all]))
 
-(deftest target-equals-scramble
+(deftest target-scramble-equal
   (testing "empty"
     (is (true? (scramble? "" ""))))
   (testing "string of length 1"
@@ -10,8 +10,23 @@
   (testing "string of length 3"
     (is (true? (scramble? "abc" "abc")))))
 
+(deftest target-scramble-equal-lenght-different-chars
+  (testing "string of length 1"
+    (is (false? (scramble? "a" "z"))))
+  (testing "string of length 3"
+    (is (false? (scramble? "abc" "xyz")))))
+
 (deftest target-is-substring-of-scramble
   (testing "target empty"
     (is (true? (scramble? "xyz" ""))))
   (testing "target is string of length 1"
-    (is (true? (scramble? "xyz" "x")))))
+    (is (true? (scramble? "xyz" "x"))))
+  (testing "target is string of length 2"
+    (is (true? (scramble? "xyz" "yz")))))
+
+(deftest target-is-not-substring-of-scramble
+  (testing "target is string of length 1"
+    (is (false? (scramble? "xyz" "a"))))
+  (testing "target is string of length 2"
+    (is (false? (scramble? "xyz" "yy")))))
+
